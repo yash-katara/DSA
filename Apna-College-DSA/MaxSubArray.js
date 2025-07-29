16c9b57255056b0515fac2417c940960c81fe248
yashkataraei <yashkataraei@gmail.com>
2025-07-29 12:32:02 +0530

    yessssssssssss

---
+++ Apna-College-DSA/MaxSubArray.js
@@ -0,0 +1,14 @@
+function MAxSubArray(arr){
+    let maxSum = arr[0];
+    let currentSum = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        currentSum = Math.max(arr[i], currentSum + arr[i]);
+        maxSum = Math.max(maxSum, currentSum);
+    }
+
+    return maxSum;
+}
+//const arr = [-2,1,-3,4,-1,2,1,-5,4];
+const arr = [1,-2,3,4,-5]
+console.log("Maximum Subarray Sum:", MAxSubArray(arr));
